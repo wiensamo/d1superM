98f7f2bbf3ef29a086959dc0405331350fbaeb1d
Wilson Sanchez <wiensamo@hotmail.com>
2023-04-13 18:30:15 -0500

    Se Actualiza el repositorio

---
+++ d1supermarket/src/components/header/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import "./header.css";
+
+const Header = () => {
+  return (
+    <nav>
+    <ul>
+      <li><a href="#">Inicio</a></li>
+      <li><a href="#">Acerca de</a></li>
+      <li><a href="#">Servicios</a></li>
+      <li><a href="#">Contacto</a></li>
+    </ul>
+  </nav>
+  )
+}
+
+export default Header
---
+++ d1supermarket/src/components/home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./home.css";
+import ProductForm from './ProductForm';
+
+const Home = () => {
+  return (
+    <div>
+      <ProductForm/>
+    </div>
+  )
+}
+
+export default Home
